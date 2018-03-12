f60d512cdac46e727d27767b08cf318393b7e5d8
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-03-12 13:29:17 -0600

    Se inicializo el proyecto con el framework ASP.Net

---
+++ bsi24tallerbilly/bsi24tallerbilly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bsi24tallerbilly.Startup))]
+namespace bsi24tallerbilly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
