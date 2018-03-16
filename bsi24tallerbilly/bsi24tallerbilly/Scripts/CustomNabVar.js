edae982d2763250b740faefc7e62c41ca30bf1a1
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-03-16 11:05:32 -0600

    Cararteristicas: * Se Organizo un poco la pagina de inicio * Se agrego el login partial para la pagina de inicio
    Falta:
    * Agregar la seccion de Galeria
    * Darle formato al Quienes somos

---
+++ bsi24tallerbilly/bsi24tallerbilly/Scripts/CustomNabVar.js
@@ -0,0 +1,17 @@
+﻿/*
+* This scrip job is keep the nabvar when the user scrolls 20px from the top of the document
+*/
+
+//Event trigger
+window.onscroll = function () { KeepNavbvar() };
+
+function KeepNavbvar() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        document.getElementById("CustomNavbar").style.top = "0";
+    }
+    else
+    {
+        document.getElementById("CustomNavbar").style.top = "-50px";
+    }
+}
