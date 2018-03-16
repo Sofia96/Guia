7776a60295775912fd9984ec9639cfcbfb28329b
Sergio Luis Segura Vidal <sergio31.96@outlook.com>
2018-03-16 11:42:04 -0600

    Se creo el controller de ContactUs

---
+++ bsi24tallerbilly/bsi24tallerbilly/Controllers/ContactUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using static bsi24tallerbilly.Models.ContactUsViewModels;
+
+namespace bsi24tallerbilly.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        [HttpPost]
+        public ActionResult SendInformation(SendEmailViewModel model)
+        {
+            //implemets the repository send email
+            return View(model);
+        }
+    }
+}
---
+++ bsi24tallerbilly/bsi24tallerbilly/Models/ContactUsViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsi24tallerbilly.Models
+{
+    public class ContactUsViewModels
+    {
+        public class SendEmailViewModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
